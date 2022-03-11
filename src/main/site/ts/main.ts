ba339fdb875ec3551c7e30af61710bf9ad1920d1
Taishi Nishizawa <taishi.nishizawa201@gmail.com>
2022-03-11 17:54:58 -0500

    done

--- src/main/site/ts/main.ts
+++ src/main/site/ts/main.ts
@@ -1,35 +1,61 @@
 // TODO: select the list element where the suggestions should go, and all three dropdown elements
 //  HINT: look at the HTML
+const sunVal: HTMLInputElement = document.querySelector("#sun") as HTMLInputElement;
+const moonVal: HTMLInputElement = document.querySelector("#moon") as HTMLInputElement;
+const risingVal: HTMLInputElement = document.querySelector("#rising") as HTMLInputElement;
+const suggestionsLst = document.querySelector("#suggestions") as HTMLInputElement;
 
 // Here, when the value of sun is changed, we will call the method postAndUpdate.
 // TODO: Do the same for moon and rising
+sunVal.addEventListener("change", postAndUpdate);
+moonVal.addEventListener("change", postAndUpdate);
+risingVal.addEventListener("change", postAndUpdate);
 
 // TODO: Define a type for the request data object here.
-// type MatchesRequestData = {}
+type MatchesRequestData = {
+  sun: string;
+  moon: string;
+  rising: string;
+}
 
 // TODO: Define a type for the response data object here.
-// type Matches = {}
+type Matches = {
+  matches: string[];
+}
 
 function postAndUpdate(): void {
   // TODO: empty the suggestionList (you want new suggestions each time someone types something new)
   //  HINT: use .innerHTML
+  suggestionsLst.innerHTML = "";
 
   // TODO: add a type annotation to make this of type MatchesRequestData
   const postParameters = {
     // TODO: get the text inside the input box
     //  HINT: use sun.value to get the value of the sun field, for example
+    sun : sunVal.value,
+    moon: moonVal.value,
+    rising: risingVal.value,
   };
 
-  console.log(postParameters)
+  console.log(postParameters);
 
   // TODO: make a POST request using fetch to the URL to handle this request you set in your Main.java
   //  HINT: check out the POST REQUESTS section of the lab and of the front-end guide.
   //  Make sure you add "Access-Control-Allow-Origin":"*" to your headers.
   //  Remember to add a type annotation for the response data using the Matches type you defined above!
-
+  fetch("http://localhost:4567/matches", {
+    method: "post",
+    body: JSON.stringify(postParameters),
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  })
   // TODO: Call and fill in the updateSuggestions method in one of the .then statements in the Promise
   //  Parse the JSON in the response object
   //  HINT: remember to get the specific field in the JSON you want to use
+  .then(response => response.json())
+  .then((data : Matches) => updateSuggestions(data.matches));
+
 }
 
 function updateSuggestions(matches: string[]): void {
@@ -38,6 +64,9 @@ function updateSuggestions(matches: string[]): void {
   //  NOTE: you should use <li> (list item) tags to wrap each element. When you do so,
   //  make sure to add the attribute 'tabindex="0"' (for example: <li tabindex="0">{your element}</li>).
   //  This makes each element selectable via screen reader.
+  for (const match of matches) {
+    suggestionsLst.innerHTML += '<li tabindex="0">' + match + '</li>';
+  }
 }
 
 // TODO: create an event listener to the document (document.addEventListener) that detects "keyup".
@@ -46,12 +75,25 @@ function updateSuggestions(matches: string[]): void {
 //  HINT: the listener callback function should be asynchronous and wait until the values are
 //  updated before calling postAndUpdate().
 
-async function updateValues(sunval: string, moonval: string, risingval: string): Promise<void>{
+const myMatchData = {
+  sun: "Cancer",
+  moon: "Gemini",
+  rising: "Aquarius",
+}
+
+document.addEventListener("keyup", (keyEntry : KeyboardEvent) => {
+  if (keyEntry.key == "q") {
+    updateValues(myMatchData.sun, myMatchData.moon, myMatchData.rising)
+    .then(promisRes => postAndUpdate());
+  }
+});
+
+async function updateValues(sunval: string, moonval: string, risingval: string): Promise<void> {
   // This line asynchronously waits 1 second before updating the values.
   // It's unnecessary here, but it simulates asynchronous behavior you often have to account for.
   await new Promise(resolve => setTimeout(resolve, 1000));
 
-  sun.value = sunval;
-  moon.value = moonval;
-  rising.value = risingval;
+  sunVal.value = sunval;
+  moonVal.value = moonval;
+  risingVal.value = risingval;
 }
